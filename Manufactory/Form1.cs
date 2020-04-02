56baae65daef5fef1c73fe8ca944eae1fe1f4718
psa480115 <34284470+spynko@users.noreply.github.com>
2020-04-02 19:54:35 +0300

    GOSHALOVISLIVY

--- Manufactory/Form1.cs
+++ Manufactory/Form1.cs
@@ -16,5 +16,20 @@ namespace Manufactory
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
