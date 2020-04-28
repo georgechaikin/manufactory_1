a91e49fd62c33eb16d6d9bb2fe33a8408aa9525b
psa480115 <34284470+spynko@users.noreply.github.com>
2020-04-28 18:24:43 +0300

    Add 3 forms

--- Manufactory/ADDNEW.cs
+++ Manufactory/ADDNEW.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Manufactory
 {
-    public partial class Form1 : Form
+    public partial class ADDNEW : Form
     {
-        public Form1()
+        public ADDNEW()
         {
             InitializeComponent();
         }
