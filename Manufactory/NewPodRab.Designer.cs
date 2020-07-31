df6ec367c5a4da6c052f3068b6fd19618beedd55
PiuPiuYbito <56234944+PiuPiuYbito@users.noreply.github.com>
2020-07-31 14:26:40 +0300

    autoscroll 2.0

--- Manufactory/NewPodRab.Designer.cs
+++ Manufactory/NewPodRab.Designer.cs
@@ -53,9 +53,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.label1.Location = new System.Drawing.Point(59, 9);
+            this.label1.Location = new System.Drawing.Point(88, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(241, 20);
+            this.label1.Size = new System.Drawing.Size(326, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Подготовительные работы";
             // 
@@ -63,18 +64,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.label2.Location = new System.Drawing.Point(27, 72);
+            this.label2.Location = new System.Drawing.Point(40, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 25);
+            this.label2.Size = new System.Drawing.Size(191, 35);
             this.label2.TabIndex = 1;
             this.label2.Text = "Фрезеровка";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 124);
+            this.label3.Location = new System.Drawing.Point(44, 191);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.Size = new System.Drawing.Size(58, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Время";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -83,9 +86,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.label4.Location = new System.Drawing.Point(27, 170);
+            this.label4.Location = new System.Drawing.Point(40, 262);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 25);
+            this.label4.Size = new System.Drawing.Size(134, 35);
             this.label4.TabIndex = 3;
             this.label4.Text = "Токарка";
             // 
@@ -93,27 +97,30 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.label5.Location = new System.Drawing.Point(27, 259);
+            this.label5.Location = new System.Drawing.Point(40, 398);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(109, 25);
+            this.label5.Size = new System.Drawing.Size(155, 35);
             this.label5.TabIndex = 4;
             this.label5.Text = "Слесарка";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(27, 215);
+            this.label6.Location = new System.Drawing.Point(40, 331);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.Size = new System.Drawing.Size(58, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "Время";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(29, 307);
+            this.label7.Location = new System.Drawing.Point(44, 472);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.Size = new System.Drawing.Size(58, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Время";
             // 
@@ -121,100 +128,112 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.label8.Location = new System.Drawing.Point(27, 347);
+            this.label8.Location = new System.Drawing.Point(40, 534);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(85, 25);
+            this.label8.Size = new System.Drawing.Size(120, 35);
             this.label8.TabIndex = 7;
             this.label8.Text = "Сварка";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(27, 402);
+            this.label9.Location = new System.Drawing.Point(40, 618);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.Size = new System.Drawing.Size(58, 20);
             this.label9.TabIndex = 8;
             this.label9.Text = "Время";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(259, 72);
+            this.label10.Location = new System.Drawing.Point(388, 111);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(75, 13);
+            this.label10.Size = new System.Drawing.Size(107, 20);
             this.label10.TabIndex = 9;
             this.label10.Text = "СТОИМОСТЬ";
             // 
             // textBox1
             // 
             this.textBox1.AccessibleName = "Под.работы/Фрезеровка/Время";
-            this.textBox1.Location = new System.Drawing.Point(75, 121);
+            this.textBox1.Location = new System.Drawing.Point(112, 186);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
             this.textBox1.TabIndex = 10;
             // 
             // textBox2
             // 
             this.textBox2.AccessibleName = "Под.работы/Токарка/Время";
-            this.textBox2.Location = new System.Drawing.Point(75, 212);
+            this.textBox2.Location = new System.Drawing.Point(112, 326);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(148, 26);
             this.textBox2.TabIndex = 11;
             // 
             // textBox3
             // 
             this.textBox3.AccessibleName = "Под.работы/Слесарка/Время";
-            this.textBox3.Location = new System.Drawing.Point(75, 304);
+            this.textBox3.Location = new System.Drawing.Point(112, 468);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(148, 26);
             this.textBox3.TabIndex = 12;
             // 
             // textBox4
             // 
             this.textBox4.AccessibleName = "Под.работы/Сварка/Время";
-            this.textBox4.Location = new System.Drawing.Point(75, 399);
+            this.textBox4.Location = new System.Drawing.Point(112, 614);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(148, 26);
             this.textBox4.TabIndex = 13;
             // 
             // textBox5
             // 
             this.textBox5.AccessibleName = "Под.работы/Фрезеровка/Стоимость";
-            this.textBox5.Location = new System.Drawing.Point(245, 121);
+            this.textBox5.Location = new System.Drawing.Point(368, 186);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.Size = new System.Drawing.Size(148, 26);
             this.textBox5.TabIndex = 14;
             // 
             // textBox6
             // 
             this.textBox6.AccessibleName = "Под.работы/Токарка/Стоимость";
-            this.textBox6.Location = new System.Drawing.Point(245, 212);
+            this.textBox6.Location = new System.Drawing.Point(368, 326);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.Size = new System.Drawing.Size(148, 26);
             this.textBox6.TabIndex = 15;
             // 
             // textBox7
             // 
             this.textBox7.AccessibleName = "Под.работы/Слесарка/Стоимость";
-            this.textBox7.Location = new System.Drawing.Point(245, 304);
+            this.textBox7.Location = new System.Drawing.Point(368, 468);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.Size = new System.Drawing.Size(148, 26);
             this.textBox7.TabIndex = 16;
             // 
             // textBox8
             // 
             this.textBox8.AccessibleName = "Под.работы/Сварка/Стоимость";
-            this.textBox8.Location = new System.Drawing.Point(245, 399);
+            this.textBox8.Location = new System.Drawing.Point(368, 614);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.Size = new System.Drawing.Size(148, 26);
             this.textBox8.TabIndex = 17;
             // 
             // addPodrabButton
             // 
             this.addPodrabButton.AccessibleName = "addPodrabButton";
-            this.addPodrabButton.Location = new System.Drawing.Point(30, 435);
+            this.addPodrabButton.Location = new System.Drawing.Point(45, 669);
+            this.addPodrabButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.addPodrabButton.Name = "addPodrabButton";
-            this.addPodrabButton.Size = new System.Drawing.Size(314, 27);
+            this.addPodrabButton.Size = new System.Drawing.Size(471, 42);
             this.addPodrabButton.TabIndex = 18;
             this.addPodrabButton.Text = "Готово";
             this.addPodrabButton.UseVisualStyleBackColor = true;
@@ -222,9 +241,10 @@
             // 
             // NewPodRab
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(367, 477);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(550, 734);
             this.Controls.Add(this.addPodrabButton);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -244,6 +264,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "NewPodRab";
             this.Text = "PodgotRabt";
             this.Load += new System.EventHandler(this.NewPodRab_Load);
